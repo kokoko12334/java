c8e052282385cb6853061bef3a5179d864d0f400
ko <kokoko12334>
2023-01-06 13:08:40 +0900

    20230106

---
+++ outputs/src/outputs/Exam2.java
@@ -0,0 +1,21 @@
+package outputs;
+
+public class Exam2 {
+	//ctrl+space  맨 위의 것
+	public static void main(String[] args) {
+		
+		 System.out.println("hellow world");
+		 System.out.println("hellow \nworld");
+		 System.out.println("1234\\123");   // "\"출력 하려면 \\두번씀
+		 
+		 System.out.println(100+100);
+			
+		 System.out.println(200.22+100);
+			
+		 System.out.println("100"+100);
+			
+		 System.out.println("200.22"+200.22);
+	}
+	
+	
+}
